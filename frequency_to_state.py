2ebe74a79fb9b5ca732978cd4131c2670552e595
Charlie Thomas <charlie.thomas@attwoodthomas.net>
2023-08-31 09:41:24 +0100

    Add phase estimation

--- frequency_to_state.py
+++ frequency_to_state.py
@@ -1,1 +1,28 @@
-p143
+"""Get QFT of a simple register."""
+from math import pi
+from qiskit import QuantumCircuit, QuantumRegister, transpile
+from qiskit.circuit.library import QFT
+from qiskit_aer import AerSimulator
+
+register = QuantumRegister(4)
+circuit = QuantumCircuit(register)
+
+circuit.x(register[0])
+circuit.x(register[1])
+circuit.barrier(register)
+
+circuit.append(QFT(num_qubits=register.size).to_instruction(), register)
+
+
+circuit.measure_all()
+
+simulator = AerSimulator()
+
+compiled_circuit = transpile(circuit, simulator)
+
+job = simulator.run(compiled_circuit, shots=1000)
+
+result = job.result()
+counts = result.get_counts(compiled_circuit)
+
+print(counts)
--- using_phase_estimation.py
+++ using_phase_estimation.py
@@ -1,1 +1,32 @@
-p160
+import math
+from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister, transpile
+from qiskit.circuit.library import PhaseEstimation
+from qiskit_aer import AerSimulator
+
+input_register = QuantumRegister(1)
+output_register = QuantumRegister(4)
+classical_output = ClassicalRegister(output_register.size)
+
+circuit = QuantumCircuit(input_register, output_register, classical_output)
+
+circuit.ry(-3*math.pi/4, input_register)
+
+hadamardCircuit = QuantumCircuit(1)
+hadamardCircuit.h(0)
+
+circuit.append(PhaseEstimation(num_evaluation_qubits=output_register.size, unitary=hadamardCircuit).to_instruction(), list(reversed([*output_register]))+[ *input_register])
+
+circuit.decompose(reps=2).draw('mpl', filename='debug.png')
+
+circuit.measure(output_register, classical_output)
+
+simulator = AerSimulator()
+
+compiled_circuit = transpile(circuit, simulator)
+
+job = simulator.run(compiled_circuit, shots=1000)
+
+result = job.result()
+counts = result.get_counts(compiled_circuit)
+
+print(counts)
